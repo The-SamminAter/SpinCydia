5ad11377297f0125f8ad50b18ddc8cd8fff99a98
The-SamminAter <48739810+The-SamminAter@users.noreply.github.com>
2020-03-12 09:25:24 -0700

    Removes something useless

--- SpinCydiaSettings/SpinCydiaSettings.h
+++
@@ -1,8 +0,0 @@
-#import <Preferences/PSListController.h>
-
-@interface SpinCydiaSettingsListController : PSListController {
-}
-@end
-
-@interface ViewController : UIViewController <UIImagePickerControllerDelegate,UINavigationControllerDelegate>
-@end
